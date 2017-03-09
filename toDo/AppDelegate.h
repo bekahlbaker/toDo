43ea1792ef3c462dfb41284e9ab7b6a52c2aa40a
bekahlbaker <bekahlbaker@gmail.com>
2017-03-09 12:17:58 -0600

    First commit

---
+++ toDo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  toDo
+//
+//  Created by Rebekah Baker on 3/9/17.
+//  Copyright © 2017 Rebekah Baker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
